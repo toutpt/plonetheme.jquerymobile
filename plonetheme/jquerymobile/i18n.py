57747f637be07321177a897406509046352a37df
toutpt <toutpt@gmail.com>
2011-01-14 13:58:12 +0100

    so many things ...

--- plonetheme/jquerymobile/i18n.py
+++ plonetheme/jquerymobile/i18n.py
@@ -4,5 +4,12 @@ messageFactory = MessageFactory("plonetheme.jquerymobile")
 
 _ = messageFactory
 
-label_domains = _(u"label_domains", default=u"Domains")
-desc_domains = _(u"desc_domains", default=u"Domains for which mobile themes will be use. One per line")
+label_domains = _(u"label_domains",
+                  default=u"Domains")
+
+desc_domains =   _(u"desc_domains",
+                   default=u"Domains for which mobile themes will be use. One per line")
+
+label_controlpanel = _(u"label_controlpanel",
+                       default=u"Jquery Mobile theme settings")
+
--- plonetheme/jquerymobile/interfaces.py
+++ plonetheme/jquerymobile/interfaces.py
@@ -7,13 +7,3 @@ from plonetheme.jquerymobile import i18n
 
 class IThemingLayer(base.IThemingLayer):
     """Browser layer"""
-
-class IThemeSettings(base.IThemeSettings):
-    """Settings for jquerymobile theme"""
-
-    domains = schema.List(
-        title=i18n.label_domains,
-        description=i18n.desc_domains,
-        value_type=schema.TextLine(),
-        required=False
-        )
