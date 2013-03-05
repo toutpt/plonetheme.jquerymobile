194f4e75638f876fda84a156898b25e8bbe97326
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-03-05 17:44:50 +0100

    add support for default_page

--- plonetheme/jquerymobile/browser/jquerymobile.py
+++ plonetheme/jquerymobile/browser/jquerymobile.py
@@ -1,22 +1,55 @@
+from zope import component
+from zope import interface
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
 from plonetheme.jquerymobile import interfaces
-from zope import interface
 
 
 class JQueryMobile(BrowserView):
     """Default browserview"""
     interface.implements(interfaces.IJqueryMobileView)
 
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.dependencies = {}
+        self.render = None
+
     def __call__(self):
         self.update()
+        if self.render:
+            return self.render
         return self.index()
 
     def update(self):
-        pass
+        #check default page
+        default_page_id = self.context.getDefaultPage()
+        if default_page_id:
+            default_page = self.context.restrictedTraverse(default_page_id)
+            if default_page:
+                render = default_page.restrictedTraverse('jquerymobile_view')()
+                self.render = render
+
+        portal_state = component.getMultiAdapter(
+            (self.context, self.request), name=u'plone_portal_state'
+        )
+        context_state = component.getMultiAdapter(
+            (self.context, self.request), name=u'plone_context_state'
+        )
+        self.dependencies['portal_state'] = portal_state
+        self.dependencies['context_state'] = context_state
 
     def pageid(self):
         return '-'.join(self.context.getPhysicalPath())[1:]
+
+    def site_title(self):
+        pstate = self.dependencies['portal_state']
+        return pstate.portal_title()
+
+
+class PloneSite(JQueryMobile):
+    pass
 
 
 class Document(JQueryMobile):
