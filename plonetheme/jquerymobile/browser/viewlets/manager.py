5f8d3c355cb65ace949db70a35f8d08cf1101527
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-05-20 19:29:31 +0200

    refactor(maintemplate): use viewlets for the below-content part
    Add viewletmanager plonetheme.jquerymobile.belowcontent
    Move footer inside it
    Move panel left & panel right inside it

--- plonetheme/jquerymobile/browser/viewlets/manager.py
+++ plonetheme/jquerymobile/browser/viewlets/manager.py
@@ -1,21 +1,25 @@
 from zope.viewlet.interfaces import IViewletManager
 
 
-class IFooter(IViewletManager):
-    """A viewlet manager that sits on the left panel"""
-
-
-class IPanelLeft(IViewletManager):
-    """A viewlet manager that sits on the left panel"""
-
-
-class IPanelRight(IViewletManager):
-    """A viewlet manager that sits on the left panel"""
-
-
 class IHeaderLeft(IViewletManager):
     """a viewlet manager that sits on the header left"""
 
 
 class IHeaderRight(IViewletManager):
     """a viewlet manager that sits on the header right"""
+
+
+class IBelowContent(IViewletManager):
+    """A viewlet manager that sits at the end of the page"""
+
+
+class IFooter(IViewletManager):
+    """A viewlet manager that sits inside IBelowContent"""
+
+
+class IPanelLeft(IViewletManager):
+    """A viewlet manager that sits inside IBelowContent"""
+
+
+class IPanelRight(IViewletManager):
+    """A viewlet manager that sits inside IBelowContent"""
