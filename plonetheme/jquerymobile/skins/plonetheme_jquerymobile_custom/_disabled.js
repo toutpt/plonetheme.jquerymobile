af991f5a7e5a9cc594c4152e31bff9c8a0b05baf
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-05-28 16:57:38 +0200

    fix(plone resources): disable resources we don't want from Plone
    Plone is full of resources which add features and styles.
    The best way to disabled resources is not to disabled them from
    portal_resources but to make them empty in a skin directory.
    This is a workaround to make it easier to uninstall the theme
    but also to create new theme based on this one.

---
+++ plonetheme/jquerymobile/skins/plonetheme_jquerymobile_custom/_disabled.js
@@ -0,0 +1,22 @@
+/*Copy/paste/rename this file to disabled a javascripts only for a theme
+ * based on this one.
+ * 
+ * Plone disabled js are:
+ * accessibility.js
+ * calendar_formfield.js
+ * collapsibleformfields.js
+ * cookie_functions.js
+ * dragdropreorder.js
+ * dropdown.js
+ * form_tabbing.js
+ * fullscreenmode.js
+ * jquery-integration.js
+ * jquery.highlightsearchterms.js
+ * kss-bbb.js
+ * livesearch.js
+ * modernizr.js
+ * nodeutilities.js
+ * popupforms.js
+ * register_function.js
+ * styleswitcher.js
+ */
