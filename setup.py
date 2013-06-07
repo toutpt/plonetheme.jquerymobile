db4fb893b99b3a554d6f7043294af611e302d9ff
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-06-07 18:05:28 +0200

    refactor: rename README.txt to rst for githbu

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ extra_requires = {'test': ['plone.app.testing', 'plone.app.robotframework']}
 setup(name='plonetheme.jquerymobile',
       version=version,
       description="jquerymobile theme",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
